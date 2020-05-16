c83a8c393ddc18d3a159241f37a03bc47c8f076c
Kozyrev Ilya <kozyrev.frontend@gmail.com>
2020-05-16 15:22:05 +0300

    добавляет блок с видео про велосипеды

--- build/js/app.js
+++ build/js/app.js
@@ -1,5 +1,6 @@
 'use strict';
 
+// Меню-бургер
 var navMain = document.querySelector('.page-header__nav');
 var navToggle = document.querySelector('.page-header__toggle');
 
@@ -22,3 +23,40 @@ inputPhone.onkeyup = function (evt) {
   var target = evt.target;
   target.value = this.value.replace(/[^\d]/g, '');
 };
+
+
+// Оптимизация вставки видео
+function findVideo() {
+  var video = document.querySelector('.video__container');
+
+  setupVideo(video);
+}
+
+function setupVideo(video) {
+  var link = document.querySelector('.video__link');
+  var button = document.querySelector('.video__button');
+
+  button.addEventListener('click', function () {
+    var iframe = createIframe();
+    var linkContainer = video.querySelector('.video__img-container');
+
+    linkContainer.removeChild(link);
+    video.removeChild(button);
+    video.appendChild(iframe);
+  });
+
+  link.removeAttribute('href');
+  video.classList.add('video__container--enabled');
+}
+
+function createIframe() {
+  var iframe = document.createElement('iframe');
+
+  iframe.setAttribute('allowfullscreen', '');
+  iframe.setAttribute('src', 'https://www.youtube.com/embed/KkVG8nCbPvU?rel=0&showinfo=0&autoplay=1');
+  iframe.classList.add('video__media');
+
+  return iframe;
+}
+
+findVideo();
