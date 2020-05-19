b3a581ad89c25ae99774d7507ad502b008970b2b
Kozyrev Ilya <kozyrev.frontend@gmail.com>
2020-05-19 11:04:09 +0300

    исправляет замечания после первого ревью наставника

--- build/js/app.js
+++ build/js/app.js
@@ -2,17 +2,26 @@
 
 // Меню-бургер
 var navMain = document.querySelector('.page-header__nav');
-var navToggle = document.querySelector('.page-header__toggle');
+var navToggle = document.querySelector('.button--toggle');
+var siteList = document.querySelector('.site-list');
 
 navMain.classList.add('page-header__nav--closed');
+navToggle.classList.add('button--nav-closed');
+siteList.classList.add('site-list--nav-closed');
 
 navToggle.addEventListener('click', function () {
   if (navMain.classList.contains('page-header__nav--closed')) {
     navMain.classList.remove('page-header__nav--closed');
     navMain.classList.add('page-header__nav--opened');
+    navToggle.classList.remove('button--nav-closed');
+    navToggle.classList.add('button--nav-opened');
+    siteList.classList.remove('site-list--nav-closed');
   } else {
     navMain.classList.add('page-header__nav--closed');
     navMain.classList.remove('page-header__nav--opened');
+    navToggle.classList.add('button--nav-closed');
+    navToggle.classList.remove('button--nav-opened');
+    siteList.classList.add('site-list--nav-closed');
   }
 });
 
@@ -21,7 +30,7 @@ var inputPhone = document.querySelector('input[type="tel"]');
 
 inputPhone.onkeyup = function (evt) {
   var target = evt.target;
-  target.value = this.value.replace(/[^\d]/g, '');
+  target.value = this.value.replace(/[a-zA-Zа-яёА-ЯЁ]/g, '');
 };
 
 
@@ -60,3 +69,12 @@ function createIframe() {
 }
 
 findVideo();
+
+// Плавный скролл
+var $page = $('html, body');
+$('a[href*="#"]').click(function () {
+  $page.animate({
+    scrollTop: $($.attr(this, 'href')).offset().top
+  }, 400);
+  return false;
+});
